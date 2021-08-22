f3eca5a56bc04078b662eed202b56caab2ff7650
Nino Joy <ninojoy1234@gmail.com>
2021-08-22 21:02:16 +0530

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-
 import About from "./About";
 import Contact from "./Contact";
 import Footer from "./Footer";
@@ -10,10 +8,6 @@ import StuffIDo from "./StuffIDo";
 export default function Home() {
 	return (
 		<div>
-			<Head>
-				<title>Nino Joy | Portfolio</title>
-			</Head>
-
 			{/* Header */}
 			<Header />
 
