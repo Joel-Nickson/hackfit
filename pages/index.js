3ba81221ce3747b57f66bee7f130d8fa393cfb53
Nino Joy <ninojoy1234@gmail.com>
2021-09-25 12:46:29 +0530

    added scroll-to-top

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,12 @@ import Footer from "./Footer";
 import Header from "./Header";
 import PersonalProjects from "./PersonalProjects";
 import StuffIDo from "./StuffIDo";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function Home() {
 	return (
 		<div>
+			<ScrollToTop />
 			{/* Header */}
 			<Header />
 
