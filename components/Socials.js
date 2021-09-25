689748d54a88e8521dbfd68baa64d18ed35baf31
Nino Joy <ninojoy1234@gmail.com>
2021-09-25 11:47:46 +0530

    updated footer

--- components/Socials.js
+++ components/Socials.js
@@ -4,7 +4,7 @@ function Socials({ link, logo, bg }) {
 	return (
 		<a href={link} target="_blank" rel="noopener noreferrer">
 			<div className="flex p-3 bg-gray-800 h-12 w-12 md:h-14 md:w-14 md:p-4 rounded-full items-center justify-center hover:bg-gray-700 sm:hover:-translate-y-1 hover:shadow-white-lg transition sm:active:translate-y-3 active:bg-gray-500">
-				<Image src={logo} width={50} height={50} />
+				<Image src={logo} width={50} height={50} priority={true} />
 			</div>
 		</a>
 	);
--- pages/Footer.js
+++ pages/Footer.js
@@ -7,28 +7,27 @@ const Footer = () => {
 	return (
 		<div
 			id="footer"
-			className=" bg-gray-900 p-5 flex items-center justify-center text-gray-100"
+			className="md:px-24 flex flex-col md:flex-row p-5 font-light font-poppins text-sm tracking-wider bg-gray-900 items-center justify-between text-gray-400"
 		>
-			<footer>
-				<p className="font-light font-poppins tracking-wider text-sm flex items-center">
-					Made with
-					<span className="mx-1.5">
-						<Image src={heart} width={18} height={18} />
-					</span>
-					using
-					{
-						<ReactTypingEffect
-							cursorClassName=""
-							speed="80"
-							eraseSpeed="100"
-							eraseDelay="1500"
-							typingDelay="500"
-							text={["React", "Next.js", "TailwindCSS"]}
-							className="ml-1"
-						/>
-					}
-				</p>
-			</footer>
+			<p className="flex items-center">
+				Made with
+				<span className="mx-1.5">
+					<Image src={heart} width={18} height={18} />
+				</span>
+				using
+				{
+					<ReactTypingEffect
+						cursorClassName=""
+						speed="80"
+						eraseSpeed="100"
+						eraseDelay="1500"
+						typingDelay="500"
+						text={["React", "Next.js", "TailwindCSS"]}
+						className="ml-1"
+					/>
+				}
+			</p>
+			<p className="pt-2 sm:pt-0">© Nino Joy</p>
 		</div>
 	);
 };
