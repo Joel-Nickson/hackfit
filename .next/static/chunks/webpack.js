74f46ce5b882c98be9563fbee772f06c1565df9e
Naveen B Jacob <naveenbjacob123@gmail.com>
2022-01-07 14:54:46 +0530

    navbar adjusted

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aa57e083e345c529"; }
+/******/ 		__webpack_require__.h = function() { return "d1f48ced43b6af2e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/sidenav.js
+++ pages/sidenav.js
@@ -6,17 +6,17 @@ const navigation = [
 ];
 function sideHeader() {
   return (
-    <div className="hidden lg:block fixed h-4/5 w-35 my-10 mt-24 rounded-2xl ml-2 bg-brochblack text-white px-6 flex flex-col justify-center content-between">
-      {navigation.map((item) => (
-        <div>
-          <a className="flex justify-center items-center">
-            {item.icon.toUpperCase()}
-          </a>
-          <div className="text-xs flex justify-center items-center">
-            <a>{item.name.toUpperCase()}</a>
+    <div className="hidden lg:block">
+      <div className="fixed h-4/5 w-35 my-10 mt-24 rounded-2xl ml-2 bg-brochblack text-white px-6 flex flex-col justify-evenly items-center">
+        {navigation.map((item) => (
+          <div>
+            <a className="flex justify-evenly">{item.icon.toUpperCase()}</a>
+            <div className="text-xs flex justify-center">
+              <a>{item.name.toUpperCase()}</a>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
