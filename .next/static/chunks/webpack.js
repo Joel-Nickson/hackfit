e6c4cd1ff1a0b42379c1f3db041d5c72d890f9f4
Naveen B Jacob <naveenbjacob123@gmail.com>
2022-01-09 10:53:30 +0530

    landing image indexed properly

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d46e447262f72b8d"; }
+/******/ 		__webpack_require__.h = function() { return "78bc788458398d69"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
